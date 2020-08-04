975af855ac693f3f53824cd98c7076b78fa2606c
0709FanLi <385717045@qq.com>
2020-08-05 01:09:48 +0800

    路由文件1.0发布了

--- src/router.js
+++ src/router.js
@@ -1,49 +1,82 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { enableScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator
-      initialRouteName="Feed"
-      tabBarOptions={{
-        activeTintColor: '#e91e63',
-      }}
-    >
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={size} />
-          ),
-          tabBarBadge: 3,
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
+    <Tab.Navigator>
+      <Tab.Screen name="Tabs" component={Tabs} />
+      <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   );
 }
 
-export default MyTabs();
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
+enableScreens();
+const Stack = createNativeStackNavigator();
+
+function Home({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('MyTabs')}
+      />
+    </View>
+  );
+}
+function Feed() {
+  return (
+    <Text>Feed</Text>
+  );
+}
+function Article() {
+  return (
+    <Text>Article</Text>
+  );
+}
+function Tabs() {
+  return (
+    <Text>Tabs</Text>
+  );
+}
+function Settings() {
+  return (
+    <Text>Settings</Text>
+  );
+}
+
+
+// const Stack = createStackNavigator();
+
+function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ Home } />
+        <Stack.Screen name="MyDrawer" component={ MyDrawer } />
+        <Stack.Screen name="MyTabs" component={ MyTabs } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
